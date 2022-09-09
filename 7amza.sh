2bb000608b7a86bf0cd0bde71083028b5f99e52e
bellihamza301 <113103376+bellihamza301@users.noreply.github.com>
2022-09-09 15:20:42 +0100

    Update 7amza.sh

--- 7amza.sh
+++ 7amza.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -zxvf lolMiner_v1.31_Lin64.tar.gz && cd 1.31 && clear && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TUXZQhaAfKoRugYLkMTzsF6JYFigaL1og8.belli --ethstratum ETHPROXY
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u COIN:WALLET_ADDRESS.WORKERNAME -p x
